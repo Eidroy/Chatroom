6ac70a057dde59389b86127bd0cb0815ffaded70
Eidroy <yordie@DESKTOP-UF3E07V>
2024-04-30 15:38:04 +0200

    Added security

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 import Login from "./pages/login";
 import Register from "./pages/register";
@@ -7,16 +13,36 @@ import Dashboard from "./pages/dashboard";
 import Chat from "./pages/chat";
 import Private from "./pages/Private";
 
+function AuthCheck() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const hasToken = !!localStorage.getItem("token");
+
+    if (!hasToken) {
+      navigate("/auth/login");
+    }
+  }, [navigate]);
+
+  return null;
+}
+
 function App() {
+  const hasToken = !!localStorage.getItem("token");
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/private-messages" element={<Private />} />
+        <Route path="*" element={<AuthCheck />} />
+        {hasToken && (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/private_messages" element={<Private />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
